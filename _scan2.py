29966bb7cbdb8251e74e71ddf49b13ff9aa51345
PolarD1115 <1335284531@qq.com>
2026-08-23 16:44:52 +0800

    chore(repo): remove local scan artifacts

--- _scan2.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import re
-src = open('server.py', encoding='utf-8').read()
-for pat in ['tarot', '塔罗', 'question']:
-    idxs = [m.start() for m in re.finditer(re.escape(pat), src)]
-    lines = [src[:i].count('\n') + 1 for i in idxs]
-    print('%s -> %s' % (pat, lines[:20]))
